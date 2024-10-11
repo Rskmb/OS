9bff732160355232f155479948b3536761bc174b
Rskmb <150017181+Rskmb@users.noreply.github.com>
2024-10-11 09:27:40 +0530

    Add files via upload

---
+++ server.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/types.h>
+
+#define MAXSIZE 27
+
+void die(char *s)
+{
+perror(s);
+exit(1);
+}
+
+
+
+int main() {
+ char c;
+ int shmid;
+ 
+ key_t key;
+ 
+ char *s,*shm;
+ key=5678;
+ 
+ if((shmid= shmget(key,MAXSIZE,IPC_CREAT | 0666))< 0)
+     die("shmget");
+ if((shm=shmat(shmid,NULL,0))== (char *) -1)
+     die("shmat");
+     
+ s=shm;
+ 
+ for(c='a';c<='z';c++)
+     *s++=c;
+     
+      while(*shm != '*')
+ sleep(1);
+  
+
+ exit(0);
+}
